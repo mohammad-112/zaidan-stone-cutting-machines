321a1cbece5b9d378f52ca7a639b3b644c03ab28
mohammad-112 <moheah333@gmail.com>
2026-01-17 11:55:58 +0300

    Delete database.js

--- database.js
+++
@@ -1,4 +0,0 @@
-// This file is NOT REQUIRED for the GitHub Pages version.
-// The database logic is now handled inside 'services/store.ts' using browser LocalStorage.
-// You can delete this file.
-module.exports = {};
